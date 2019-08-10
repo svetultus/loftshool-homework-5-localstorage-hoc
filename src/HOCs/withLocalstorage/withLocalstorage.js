4bde3b144e607d671e9635fc089712c1115fb15a
svetultus <svetultus@gmail.com>
2019-08-10 21:40:21 +0300

    main functions

--- src/HOCs/withLocalstorage/withLocalstorage.js
+++ src/HOCs/withLocalstorage/withLocalstorage.js
@@ -1,6 +1,35 @@
 import React, { Component } from 'react';
 import { load, save } from '../../localstorage';
 
-const withLocalstorage = () => () => {};
+const withLocalstorage = (
+  storageName,
+  dataToSave,
+  ...rest
+) => WrappedComponent => {
+  class withLocalstorage extends React.Component {
+    constructor(props) {
+      super(props);
+      this.dataFromStorage = this.loadData(storageName);
+    }
+
+    loadData(storageName) {
+      return load(storageName);
+    }
+
+    saveData(dataToSave) {
+      save(storageName, dataToSave);
+    }
+    render() {
+      return (
+        <WrappedComponent
+          savedData={this.dataFromStorage}
+          saveData={this.saveData}
+          {...rest}
+        />
+      );
+    }
+  }
+  return withLocalstorage;
+};
 
 export default withLocalstorage;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,18 @@
 import React, { PureComponent } from 'react';
 import './App.css';
 import Todo from '../Todo';
+import Card from '../Card';
 
 class App extends PureComponent {
   render() {
     return (
       <main className="main">
         <div className="main__cell">
-          <Todo />
+          <Card title="Список дел">
+            <div className="todo t-todo-list">
+              <Todo />
+            </div>
+          </Card>
         </div>
       </main>
     );
